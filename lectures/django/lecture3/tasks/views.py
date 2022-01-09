e0e2ea723e4e83f7c0845071d94a604d5bb4af34
Samuel <samuelsneyd@gmail.com>
2022-01-09 18:17:57 +1300

    Finish Lecture 3 code. Import Project 1: Wiki folder

--- lectures/django/lecture3/tasks/views.py
+++ lectures/django/lecture3/tasks/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django import forms
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label='New Task',
+    widget=forms.TextInput(attrs={
+        'class': 'input-class',
+        'id': 'task-input',
+        'autocomplete': 'off'
+    }))
+
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
+    return render(request, 'tasks/index.html', {
+        'tasks': request.session['tasks']
+    })
+
+
+def add(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session['tasks'] += [task]
+            return redirect(reverse('tasks:index'))
+        else:
+            return render(request, 'tasks/add.html', {
+                'form': form
+            })
+    return render(request, 'tasks/add.html', {
+        'form': NewTaskForm()
+    })
+
