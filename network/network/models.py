e7d306f7aad22083bd1d5a24cc248894dea3972c
Samuel <samuelsneyd@gmail.com>
2022-06-26 10:31:51 +1200

    Init models and migration

--- network/network/models.py
+++ network/network/models.py
@@ -3,4 +3,35 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    """Users of the application"""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    """A text post, created by users"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    text = models.CharField(max_length=280)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Like(models.Model):
+    """A like from a user on a post"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    """Comments from a user on a post"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    text = models.CharField(max_length=280)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
