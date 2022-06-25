cc4cfa6d1a9174f8e1e2c8ba9b466796788e7532
Samuel <samuelsneyd@gmail.com>
2022-06-26 11:36:11 +1200

    Add new post and posts HTML
    Add placeholder post for index view.
    
    Format HTML templates

--- network/network/views.py
+++ network/network/views.py
@@ -4,11 +4,19 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
 
 
 def index(request):
-    return render(request, "network/index.html")
+    posts = [{
+        "user": "user",
+        "text": "text",
+        "created_at": "created_at",
+        "likes": 0
+    }]
+    return render(request, "network/index.html", {
+        "posts": posts
+    })
 
 
 def login_view(request):
